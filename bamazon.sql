ea17f310ed75c24a28679d43729a6b35827446a3
rknucks <42450529+rknucks@users.noreply.github.com>
2019-02-11 15:14:44 -0700

    adding code to customer.js

--- bamazon.sql
+++ bamazon.sql
@@ -1,1 +1,26 @@
-SELECT * FROM bamazon.products;
+CREATE DATABASE Bamazon;
+
+USE Bamazon;
+
+CREATE TABLE Products(
+    ID INTEGER AUTO_INCREMENT NOT NULL,
+    ProductName VARCHAR(100) NOT NULL,
+    DepartmentName VARCHAR(50) NOT NULL,
+    Price DECIMAL(4,2) NOT NULL,
+    StockQuantity INT(10) NOT NULL,
+    primary key(ID)
+);
+
+select * from Products;
+
+INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity)
+VALUES  ("Broncos Hat","Hats",24.99,10),
+        ("Nuggets Hat","Hats",12.95,8),
+        ("Broncos Sweatshirt","Apparel",49.95,5),
+        ("Broncos T-Shirt","Apparel",22.99,12),
+        ("Nuggets T-Shirt","Apparel",12.50,1),
+        ("Rockies Poster","Home and Office",6.95,5),
+        ("Rockies Hat","Hats",32.00,6),
+        ("Broncos Clock","Home and Office",121.95,3),
+        ("Avalanche Towel","Bed and Bath",9.98,5),
+        ("Avalanche Sheets","Bed and Bath",16.99,2);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,3 +13,105 @@ var connection = mysql.createConnection({
 	password: "",
 	database: 'bamazon'
 });
+
+function start(){
+	//showing the list of all the items
+	connection.query('SELECT * FROM Products', function(err, res){
+	  if(err) throw err;
+	
+	  console.log('This is bamazon')
+	  console.log('----------------------------------------------------------------------------------------------------')
+	
+	  for(var i = 0; i<res.length;i++){
+		console.log("ID: " + res[i].id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "QTY: " + res[i].stock_quantity);
+		console.log('--------------------------------------------------------------------------------------------------')
+	  }
+	  //prompt user to choose am item
+	  console.log(' ');
+	  inquirer.prompt([
+		{
+		  type: "input",
+		  name: "id",
+		  message: "Choose the ID number of the item you wish to buy?",
+		  //validates the ID
+		  validate: function(value){
+			if(isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0){
+			  return true;
+			} else{
+			  return false;
+			}
+		  }
+		},
+		//asks user how many items to buy and validates number
+		{
+		  type: "input",
+		  name: "qty",
+		  message: "How many would you like to buy?",
+		  validate: function(value){
+			if(isNaN(value)){
+			  return false;
+			} else{
+			  return true;
+			}
+		  }
+		}
+		]).then(function(ans){
+		  var itemToBuy = (ans.id)-1;
+		  var ammountToBuy = parseInt(ans.qty);
+		  var total = parseFloat(((res[itemToBuy].price)*ammountToBuy).toFixed(2));
+	
+		  //checks if enough items are in stock
+		  if(res[itemToBuy].stock_quantity >= ammountToBuy){
+			//updates quantity in Products
+			connection.query("UPDATE Products SET ? WHERE ?", [
+			{stock_quantity: (res[itemToBuy].stock_quantity - amountToBuy)},
+			{id: ans.id}
+			], function(err, result){
+				if(err) throw err;
+				console.log("Thanks for your purchase! Your total is $" + total.toFixed(2));
+			});
+	
+			connection.query("SELECT * FROM Departments", function(err, deptRes){
+			  if(err) throw err;
+			  var index;
+			  for(var i = 0; i < deptRes.length; i++){
+				if(deptRes[i].department_name === res[itemToBuy].department_name){
+				  index = i;
+				}
+			  }
+			  
+			  
+			  connection.query("UPDATE Departments SET ? WHERE ?", [
+			  {TotalSales: deptRes[index].TotalSales + total},
+			  {department_name: res[itemToBuy].department_name}
+			  ], function(err, deptRes){
+				  if(err) throw err;
+				  
+			  });
+			});
+	
+		  } else{
+			console.log("We're sorry, we don't have that many in stock");
+		  }
+	
+		  reprompt();
+		})
+	})
+	}
+	
+	//asks if they would like to purchase another item
+	function reprompt(){
+	  inquirer.prompt([{
+		type: "confirm",
+		name: "reply",
+		message: "Would you like to purchase another item?"
+	  }]).then(function(ans){
+		if(ans.reply){
+		  start();
+		} else{
+		  console.log("See you soon!");
+		}
+	  });
+	}
+	
+	start();
